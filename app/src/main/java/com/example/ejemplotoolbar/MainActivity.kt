b08dc94da34edf6ff9c98971eb81079f7c7cfa5a
HeynerPat <72895524+HeynerPat@users.noreply.github.com>
2022-03-06 22:38:52 -0500

    SearchView

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.ejemplotoolbar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
@@ -32,7 +34,34 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
+
+        val itemBusqueda = menu?.findItem(R.id.busqueda)
+        var vistaBusqueda = itemBusqueda?.actionView as SearchView
+
+        vistaBusqueda.queryHint = "Escribe tu nombre..."
+
+        vistaBusqueda.setOnQueryTextFocusChangeListener{ view, b ->
+            Log.d("LISTENERFOCUS", hasWindowFocus().toString())
+        }
+
+        vistaBusqueda.setOnQueryTextListener(object :  SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(p0: String?): Boolean {
+                if(p0 != null){
+                    Log.d("onQueryTextChange",p0)
+                }
+                return true
+            }
+
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                if(p0 != null){
+                    Log.d("onQueryTextSubmit",p0)
+                }
+                return true
+            }
+        })
+
         return super.onCreateOptionsMenu(menu)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
